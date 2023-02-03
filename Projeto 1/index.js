de4b41f306a7883811fb681430fe4220e561d88c
Wallace Ketler <56899474+wallaceketler@users.noreply.github.com>
2023-02-02 23:54:47 -0300

    Atualização Projeto 1

--- Projeto 1/index.js
+++ Projeto 1/index.js
@@ -7,29 +7,15 @@ app.set('view engine', 'ejs')
 app.use(express.static('public'))
 
 //ao usar o render ele já olha para o diretório views por padrão
-app.get('/:nome/:lang', (req,res) => {
+app.get('/', (req,res) => {
 
-    //as variáveis nome e lang estão vindo dos parâmetros de rotas
-    var nome = req.params.nome
-    var lang = req.params.lang
-    var exibirMsg = false
-    var produtos = [
-        {nome: "Doritos", preco: 3.14},
-        {nome: "Coca-cola", preco: 5},
-        {nome: "Leite", preco: 1.45},
-        {nome: "Carne", preco: 15},
-        {nome: "Redbull", preco: 6},
-        {nome: "Nescau", preco: 4},
-    ]
-    //ao renderizarmos e colocarmos as variáveis que queremos renderizar, enviaremos para o EJS
-    res.render("index",{
-        nome: nome,
-        lang: lang,
-        empresa: "Empresa teste",
-        msg: exibirMsg,
-        produtos: produtos,
-    })
+   
+    res.render("index",{})
 
+})
+
+app.get("/perguntar", (req, res) =>{
+    res.render("perguntar",{})
 })
 
 //inicia o servidor na porta desejada
